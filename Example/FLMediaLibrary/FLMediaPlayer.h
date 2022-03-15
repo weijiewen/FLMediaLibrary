999ddba8a1a94018031251b25ad39a27456a325e
weijiwen <txywjw@icloud.com>
2022-03-15 18:11:03 +0800

    提交

--- Example/FLMediaLibrary/FLMediaPlayer.h
+++ Example/FLMediaLibrary/FLMediaPlayer.h
@@ -32,8 +32,32 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)playerTimeChange:(FLMediaPlayer *)player currentSeconds:(NSInteger)currentSeconds duration:(NSInteger)duration;
 @end
 
+@protocol FLMediaPlayerDataSource <NSObject>
+@optional
+
+/// 播放器需要请求视频数据
+/// @param player player description
+/// @param request request description
+/// @param response 收到返回的response后调用该block
+/// @param appendData 收到数据调用该block追加数据
+/// @param finish 本次请求结束
+- (void)playerWillRequest:(FLMediaPlayer *)player
+                  request:(NSURLRequest *)request
+                 response:(void(^)(NSURLResponse *response))response
+               appendData:(void(^)(NSData *data))appendData
+                   finish:(dispatch_block_t)finish;
+
+/// 播放器已取消请求
+/// @param player player description
+/// @param request request description
+- (void)playerCancelRequest:(FLMediaPlayer *)player
+                    request:(NSURLRequest *)request;
+@end
+
 @interface FLMediaPlayer : NSObject
 @property (nonatomic, weak) id <FLMediaPlayerDelegate> delegate;
+/// 播放器下载代理，不设置该代理则使用NSURLSession http下载，   如视频资源在阿里云OSS私有空间时使用该代理
+@property (nonatomic, weak) id <FLMediaPlayerDataSource> dataSource;
 /// 是否加载后自动开始播放，默认YES
 @property (nonatomic, assign) BOOL autoPlay;
 /// 是否循环播放，默认NO
--- Example/FLMediaLibrary/FLMediaPlayer.m
+++ Example/FLMediaLibrary/FLMediaPlayer.m
@@ -6,8 +6,196 @@
 //  Copyright © 2022 weijiwen. All rights reserved.
 //
 
+#import <objc/runtime.h>
 #import <AVFoundation/AVFoundation.h>
 #import "FLMediaPlayer.h"
+
+static NSString *const kFLMediaPlayerScheme = @"TCKJPlay";
+
+@interface NSURLSessionDataTask (FLMediaDownloader)
+@property (nonatomic, strong) AVAssetResourceLoadingRequest *fl_loadingRequest;
+@property (nonatomic, strong) NSMutableData *fl_cacheData;
+@end
+@implementation NSURLSessionDataTask (FLMediaDownloader)
+- (void)setFl_loadingRequest:(AVAssetResourceLoadingRequest *)fl_loadingRequest {
+    objc_setAssociatedObject(self, @selector(fl_loadingRequest), fl_loadingRequest, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+- (AVAssetResourceLoadingRequest *)fl_loadingRequest {
+    return objc_getAssociatedObject(self, _cmd);
+}
+- (void)setFl_cacheData:(NSMutableData *)fl_cacheData {
+    objc_setAssociatedObject(self, @selector(fl_cacheData), fl_cacheData, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+- (NSMutableData *)fl_cacheData {
+    return objc_getAssociatedObject(self, _cmd);
+}
+@end
+
+
+@interface FLMediaDownloadCache : NSObject
+@property (nonatomic, strong) dispatch_semaphore_t fileLock;
+@property (nonatomic, strong) NSURL *URL;
+- (NSString *)directoryPath;
+- (NSString *)listPath;
+- (NSString *)dataPathWithStart:(long)start end:(long)end;
+- (NSMutableArray *)list;
+@end
+
+@implementation FLMediaDownloadCache
+
+- (void)dealloc {
+    while (dispatch_semaphore_signal(self.fileLock)) {}
+}
+
++ (instancetype)cacheWithURL:(NSURL *)URL {
+    FLMediaDownloadCache *cache = FLMediaDownloadCache.alloc.init;
+    cache.fileLock = dispatch_semaphore_create(1);
+    cache.URL = URL;
+    return cache;
+}
+
+- (NSString *)directoryPath {
+    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject stringByAppendingPathComponent:@"FLMediaPlayerVideo"];
+    NSString *base64String = [[self.URL.absoluteString dataUsingEncoding:NSUTF8StringEncoding] base64EncodedStringWithOptions:0];
+    path = [path stringByAppendingPathComponent:base64String];
+    BOOL isDirectory = NO;
+    BOOL exists = [NSFileManager.defaultManager fileExistsAtPath:path isDirectory:&isDirectory];
+    if (!exists || !isDirectory) {
+        [NSFileManager.defaultManager createDirectoryAtPath:path withIntermediateDirectories:YES attributes:@{} error:nil];
+    }
+    return path;
+}
+
+- (NSString *)listPath {
+    return [self.directoryPath stringByAppendingPathComponent:@"caches"];
+}
+
+- (NSString *)dataPathWithStart:(long)start end:(long)end {
+    return [self.directoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%ld-%ld", start, end]];
+}
+
+- (NSMutableArray *)list {
+    NSMutableArray *cacheList = NSMutableArray.array;
+    NSString *base64String = [NSString stringWithContentsOfFile:self.listPath encoding:NSUTF8StringEncoding error:nil];
+    if (base64String.length) {
+        NSData *data = [NSData.alloc initWithBase64EncodedString:base64String options:0];
+        NSArray *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+        if (json) {
+            cacheList = json.mutableCopy;
+        }
+    }
+    return cacheList;
+}
+
+// 1.
+//新：  |-----|
+//旧：     |-----|
+//
+//
+// 2.
+//新：     |-----|
+//旧：  |-----|
+//
+//
+// 3.
+//新：      |-----|
+//旧：  |-----|
+//
+//
+// 4.
+//新：       |----------|
+//旧：  |-----|  ...  |-----|
+
+- (void)cacheData:(NSData *)data start:(long)start {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        dispatch_semaphore_wait(self.fileLock, DISPATCH_TIME_FOREVER);
+        NSMutableData *mutableData = [NSMutableData dataWithData:data];
+        long end = start + mutableData.length - 1;
+        NSString *dataPath = [self dataPathWithStart:start end:end];
+        NSMutableArray *cacheList = self.list;
+        if (!cacheList.count) {
+            [mutableData writeToFile:dataPath atomically:YES];
+            [cacheList addObject:NSStringFromRange(NSMakeRange(start, mutableData.length))];
+            if ([NSFileManager.defaultManager fileExistsAtPath:self.listPath]) {
+                [NSFileManager.defaultManager removeItemAtPath:self.listPath error:nil];
+            }
+            [cacheList writeToFile:self.listPath atomically:YES];
+        }
+        else {
+            NSMutableArray *<>
+        }
+        dispatch_semaphore_signal(self.fileLock);
+    });
+}
+
+@end
+
+
+@interface FLMediaDownloader : NSObject <NSURLSessionDataDelegate>
+@property (nonatomic, weak) FLMediaPlayer *player;
+@property (nonatomic, strong) NSMutableDictionary <NSString *, NSURLSessionDataTask *> *tasks;
+@property (nonatomic, strong) NSURLSession *session;
+@end
+
+@implementation FLMediaDownloader
+
++ (instancetype)downloaderWithPlayer:(FLMediaPlayer *)player {
+    FLMediaDownloader *downloader = FLMediaDownloader.alloc.init;
+    downloader.player = player;
+    return downloader;
+}
+
+- (NSURLSession *)session {
+    if (!_session) {
+        NSURLSessionConfiguration *config = NSURLSessionConfiguration.defaultSessionConfiguration;
+        config.networkServiceType = NSURLNetworkServiceTypeVideo;
+        _session = [NSURLSession sessionWithConfiguration:config];
+    }
+    return _session;
+}
+
+- (void)downloadWithRequest:(AVAssetResourceLoadingRequest *)request {
+    NSURL *URL = request.request.URL;
+    if ([URL.absoluteString hasPrefix:kFLMediaPlayerScheme]) {
+        NSString *base64String = [URL.absoluteString stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"%@://", kFLMediaPlayerScheme] withString:@""];
+        NSData *data = [NSData.alloc initWithBase64EncodedString:base64String options:0];
+        NSString *urlString = [NSString.alloc initWithData:data encoding:NSUTF8StringEncoding];
+        URL = [NSURL URLWithString:urlString];
+    }
+    
+    NSMutableDictionary *allHTTPHeaderFields = request.request.allHTTPHeaderFields.mutableCopy;
+    NSMutableURLRequest *mutableRequest = [NSMutableURLRequest requestWithURL:URL];
+    
+    NSURLSessionDataTask *task = [self.session dataTaskWithRequest:mutableRequest];
+    task.fl_cacheData = NSMutableData.data;
+    task.fl_loadingRequest = request;
+    [self.tasks setObject:task forKey:request.request.URL.absoluteString];
+    [task resume];
+}
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler {
+    dataTask.fl_loadingRequest.response = response;
+    !completionHandler ?: completionHandler(NSURLSessionResponseAllow);
+}
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data {
+    [dataTask.fl_cacheData appendData:data];
+    [dataTask.fl_loadingRequest.dataRequest respondWithData:data];
+}
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(nullable NSError *)error {
+    if ([task isKindOfClass:NSURLSessionDataTask.class]) {
+        NSURLSessionDataTask *dataTask = (NSURLSessionDataTask *)task;
+        if (error) {
+            [dataTask.fl_loadingRequest finishLoadingWithError:error];
+        }
+        else {
+            [dataTask.fl_loadingRequest finishLoading];
+        }
+    }
+}
+
+@end
 
 @interface FLMediaPlayer ()
 @property (nonatomic, strong) AVPlayerItem *item;
@@ -15,6 +203,22 @@
 @property (nonatomic, strong) AVPlayerLayer *playerLayer;
 @property (nonatomic, strong) id timeObserver;
 @end
+
+@interface FLMediaPlayer (AVAssetResourceLoaderDelegate) <AVAssetResourceLoaderDelegate>
+
+@end
+@implementation FLMediaPlayer (AVAssetResourceLoaderDelegate)
+
+- (BOOL)resourceLoader:(AVAssetResourceLoader *)resourceLoader shouldWaitForLoadingOfRequestedResource:(AVAssetResourceLoadingRequest *)loadingRequest {
+    NSURL *URL = loadingRequest.request.URL;
+    if ([URL.scheme isEqualToString:kFLMediaPlayerScheme]) {
+        
+    }
+    return YES;
+}
+
+@end
+
 @implementation FLMediaPlayer
 
 - (void)dealloc {
@@ -63,7 +267,9 @@
             }
                 break;
             default: {
-                
+                if ([self.delegate respondsToSelector:@selector(playerFailure:error:)]) {
+                    [self.delegate playerFailure:self error:self.player.error];
+                }
             }
                 break;
         }
